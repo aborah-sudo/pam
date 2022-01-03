47bed138a34901d32a8a8ac621ce23011bff9644
Anuj Borah <aborah@redhat.com>
2022-01-03 15:19:44 +0530

    Fix

--- multihost_test/bz_automation/test_bz_automation.py
+++ multihost_test/bz_automation/test_bz_automation.py
@@ -1,5 +1,6 @@
 
 import pytest
+from sssd.testlib.common.utils import SSHClient
 
 
 def execute_cmd(multihost, command):
@@ -8,6 +9,7 @@ def execute_cmd(multihost, command):
 
 
 @pytest.mark.tier1
+@pytest.mark.usefixtures('unlimited_ssh')
 class TestPamBz(object):
     def test_unlimited(self, multihost):
         """
@@ -15,15 +17,21 @@ class TestPamBz(object):
         :id: 54d940a6-540d-11ec-9a01-845cf3eff344
         :bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1989900
         """
-        execute_cmd(multihost, "cp -vf /etc/security/limits.conf /tmp/limits.conf_anuj")
-        user = "anuj_test"
-        execute_cmd(multihost, "yum update -y pam")
-        execute_cmd(multihost, "useradd anuj_test")
-        execute_cmd(multihost, f"echo password123 | passwd --stdin {user}")
-        execute_cmd(multihost, 'echo "anuj_test hard nofile -1" >> "/etc/security/limits.conf"')
         execute_cmd(multihost, "su -c 'id' anuj_test > /tmp/anuj")
         cmd = execute_cmd(multihost, "cat /tmp/anuj")
         for id_id in ['uid=', 'gid=', 'groups=', 'anuj_test']:
             assert id_id in cmd.stdout_text
-        execute_cmd(multihost, "cp -vf /tmp/limits.conf_anuj /etc/security/limits.conf")
-        execute_cmd(multihost, "userdel -rf anuj_test")
+
+    def test_unlimited_ssh(self, multihost):
+        """
+        :title: For nofile, a value of "unlimited" should be allowed.
+        :id: 54d940a6-540d-11ec-9a01-845cf3eff344
+        :bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1989900
+        """
+        user = "anuj_test"
+        client_e = multihost.client[0].ip
+        ssh1 = SSHClient(client_e, username=user, password="password123")
+        (result1, result2, result3) =  ssh1.execute_cmd("id")
+        ssh1.close()
+        for id_id in ['uid=', 'gid=', 'groups=', 'anuj_test']:
+            assert id_id in result1.read()
