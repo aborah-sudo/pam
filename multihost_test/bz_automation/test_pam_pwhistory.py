e0826783b8a86ba2ba1f5d076089be482ac8c5f2
aborah <aborah@redhat.com>
2024-06-19 18:46:50 +0530

    add

--- multihost_test/bz_automation/test_pam_pwhistory.py
+++ multihost_test/bz_automation/test_pam_pwhistory.py
@@ -18,6 +18,7 @@ class TestPamBz(object):
         :id: 47e71a8e-2203-11ed-968a-845cf3eff344
         :bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2068461
         """
+        client = multihost.client[0]
         execute_cmd(multihost, "sed -i "
                                "'x;/./{x;b};x;/password/h;"
                                "//ipassword\trequired\tpam_pwhistory.so' "
@@ -26,24 +27,24 @@ class TestPamBz(object):
             execute_cmd(multihost, f"echo {data} >> /etc/security/pwhistory.conf")
         # Try same password continually
         execute_cmd(multihost, f"echo x86_64_baseos_rpms | passwd --stdin local_anuj")
-        assert "failed" in \
-               multihost.client[0].run_command(f"echo x86_64_baseos_rpms | "
-                                               f"passwd --stdin local_anuj").stderr_text
+        command = client.run_command(
+            f"echo x86_64_baseos_rpms | passwd --stdin local_anuj", raiseonerr=False).stderr_text
+        assert "failed" in command or "exhausted" in command
         # Try same password after 2nd time
         for passwd in ['x86#64#baseos#rpms',
                        'x86^64^baseos^rpms']:
             execute_cmd(multihost, f"echo {passwd} | passwd --stdin local_anuj")
-        assert "failed" in \
-               multihost.client[0].run_command(f"echo x86#64#baseos#rpms | "
-                                               f"passwd --stdin local_anuj").stderr_text
+        command = multihost.client[0].run_command(
+            f"echo x86#64#baseos#rpms | passwd --stdin local_anuj", raiseonerr=False).stderr_text
+        assert "failed" in command or "exhausted" in command
         # Try same password after 3rd time
         for passwd in ['HI_I_AM_ANUJ',
                        'HI#I#AM#ANUJ',
                        'HI^I^AM^ANUJ']:
             execute_cmd(multihost, f"echo {passwd} | passwd --stdin local_anuj")
-        assert "failed" in \
-               multihost.client[0].run_command(f"echo HI_I_AM_ANUJ | "
-                                               f"passwd --stdin local_anuj").stderr_text
+        command = client.run_command(
+            f"echo HI_I_AM_ANUJ | passwd --stdin local_anuj", raiseonerr=False).stderr_text
+        assert "failed" in command or "exhausted" in command
         # Try same password after 4th time
         for passwd in ['ANUJ_AM_I_HI',
                        'ANUJ#AM#I#HI',
